29d06ed8ea05d6652163fdb82aae38e48f10c148
Benjamin Van Treese <vantreeseba@gmail.com>
2018-07-27 18:04:23 -0500

    Add webpack build and dist for browser

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// Define the Webpack config.
+const config = {
+  devtool: 'inline-source-map',
+  performance: {
+    hints: false,
+  },
+  entry: {
+    index: [
+      './index.js',
+    ],
+  },
+  module: {},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js?[chunkhash]',
+  },
+  plugins: [
+    // Use hoisting.
+    new webpack.optimize.ModuleConcatenationPlugin(),
+  ],
+};
+
+// Define development-only plugins.
+// if (isDev) {
+//   // Setup the source maps.
+//   config.plugins.push(new webpack.SourceMapDevToolPlugin({
+//     filename: '[file].map',
+//   }));
+// }
+
+module.exports = config;
