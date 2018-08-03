d70073f512db6ff0753089a133df496dc46a53ec
Benjamin Van Treese <vantreeseba@gmail.com>
2018-08-03 16:50:41 -0500

    Add library expose to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
-const webpack = require('webpack');
 
 // Define the Webpack config.
 const config = {
-  devtool: 'inline-source-map',
   performance: {
     hints: false,
   },
@@ -12,24 +10,13 @@ const config = {
       './index.js',
     ],
   },
-  module: {},
   output: {
+    library: ['poissonDiscSampler'],
+    libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: '[name].js?[chunkhash]',
   },
-  plugins: [
-    // Use hoisting.
-    new webpack.optimize.ModuleConcatenationPlugin(),
-  ],
 };
-
-// Define development-only plugins.
-// if (isDev) {
-//   // Setup the source maps.
-//   config.plugins.push(new webpack.SourceMapDevToolPlugin({
-//     filename: '[file].map',
-//   }));
-// }
 
 module.exports = config;
